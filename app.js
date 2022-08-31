f66560e405496977b2f7597f06bc1b912adb8279
Filip Smyczek <smyczekf@interia.pl>
2022-08-31 17:31:33 +0200

    Template finished v2

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
-hamburger.addEventListener('click',()={
+hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 });
 
-document.addEventListener('scroll',()={
+document.addEventListener('scroll',()=>{
     let scroll_position = window.scrollY;
-    if(scroll_position  250){
+    if(scroll_position > 250){
         header.style.backgroundColor = '#29323c';
     }
     else{
@@ -19,7 +19,7 @@ document.addEventListener('scroll',()={
 });
 
 menu_item.forEach((item = {
-    item.addEventListener('click',()={
+    item.addEventListener('click',()=>{
         hamburger.classList.toggle('active');
         mobile_menu.classList.toggle('active');
     })
