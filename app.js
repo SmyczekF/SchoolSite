896286233f95fba120db481bb8b3a50f52cc280f
Filip Smyczek <smyczekf@interia.pl>
2022-09-13 16:57:02 +0200

    menu fixed, new pages added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const website = document.querySelector('html');
 const methods_menu = document.querySelectorAll('.header .teach-meth-menu');
 const help_menu = document.querySelectorAll('.header .help-menu');
 
+
 hamburger.addEventListener('click',()=>{
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
@@ -13,7 +14,7 @@ hamburger.addEventListener('click',()=>{
 
 
 document.addEventListener('scroll',()=>{
-    let scroll_position = window.scrollY;
+    var scroll_position = window.scrollY;
     
     if(scroll_position > 600 && scroll_position < 1600){
         website.style.backgroundColor = "#e1f4f5";
@@ -34,51 +35,62 @@ document.addEventListener('scroll',()=>{
         website.style.backgroundColor = "#fcc0cf";
     }
 });
-var isClickedTeach = false;
-var isClickedHelp = false;
+
+var sub_menus = {'teach-meth': [methods_menu, false], 'help': [help_menu, false]};
+var sub_menus_els = ['teach-meth-menu', 'help-menu'];
 
 menu_item.forEach((item => {
+    // if(width < 1200){
     item.addEventListener('click',()=>{
-        
-        if(item.className != 'teach-meth' && item.className != 'help'){
+        if(!(item.className in sub_menus)){
             hamburger.classList.toggle('active');
             mobile_menu.classList.toggle('active');
         }
         
-        if(item.className === 'teach-meth' && isClickedTeach === false){
-            isClickedTeach = !isClickedTeach;
+        if(item.className in sub_menus && sub_menus[item.className][1] === false){
+            sub_menus[item.className][1] = !sub_menus[item.className][1];
             item.classList.toggle('active');
         }
-        else if (item.className === 'help' && isClickedHelp === false){
-            isClickedHelp = !isClickedHelp;
-            item.classList.toggle('active');
-        }
-        else if (item.className === 'teach-meth' && isClickedTeach === true){
-            isClickedTeach = !isClickedTeach;
-            methods_menu.forEach((el => {
-                el.style.display = 'none';
-            }));
-        }
-        else if (item.className === 'help' && isClickedHelp === true){
-            isClickedHelp = !isClickedHelp;
-            help_menu.forEach((el => {
+        if(item.className in sub_menus && sub_menus[item.className][1] === true){
+            sub_menus[item.className][1] = !sub_menus[item.className][1];
+            sub_menus[item.className][0].forEach((el => {
                 el.style.display = 'none';
             }));
         }
 
-        if(item.className ==='teach-meth active'){
-            methods_menu.forEach((el => {
-                el.style.display = 'list-item';
-            }));
-
-            item.classList.toggle('active');
-        }
-        else if (item.className ==='help active'){
-            help_menu.forEach((el => {
+        if(item.className.includes('active')){
+            let temp_str = item.className.substring(0, item.className.length - 7);
+            sub_menus[(temp_str)][0].forEach((el => {
                 el.style.display = 'list-item';
             }));
 
             item.classList.toggle('active');
         }
     })
+    // }
+    // else{
+    // item.addEventListener('mouseenter',()=>{
+    
+    //     if(item.className in sub_menus){
+    //         sub_menus[item.className][0].forEach((el => {
+    //             el.style.display = 'list-item';
+    //         }))
+        
+        
+    //     }
+    // })
+    // item.addEventListener('mouseleave',()=>{
+              
+    //     if(item.className in sub_menus){
+    //         sub_menus[item.className][0].forEach((el => {
+    //             el.style.display = 'none';
+    //         }))
+    //     }
+    // })
+    // }
+    
 }));
+
+// menu_item.forEach((item => {
+    
+// }));
